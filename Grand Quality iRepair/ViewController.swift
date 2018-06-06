ca3f54c86a2bad489874ef09f1390fd61a157921
William Rozier <williamrozier@icloud.com>
2018-06-06 00:49:47 -0400

    Initial Commit

---
+++ Grand Quality iRepair/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Grand Quality iRepair
+//
+//  Created by William Roizer on 6/6/18.
+//  Copyright © 2018 William Roizer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
